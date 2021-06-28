8e1f1b955572f3fb6fd4a177d68794d22865c522
Khan <assassin.13.black@gmail.com>
2021-06-28 19:54:17 +0300

    feat(Singleton): disabled copy and assign operators

--- Paterns/Singleton.h
+++ Paterns/Singleton.h
@@ -3,6 +3,11 @@ class Singleton
 {
 public:
 	static Singleton& getInstance();
+
+	// disable copy and assign operators
+	Singleton(Singleton& other) = delete;
+	void operator=(const Singleton& other) = delete;
+
 private:
 	inline static Singleton* instance = nullptr; 
 
